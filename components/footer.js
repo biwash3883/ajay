81f1f998a44f0b21410b5cc71e94821c86fc9ab1
biwashpra <biwashpra3883@gmail.com>
2024-02-23 17:01:45 +0545

    refact: remove unnecessary

--- components/footer.js
+++ components/footer.js
@@ -91,18 +91,12 @@ export default function Footer() {
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
           Copyright © {new Date().getFullYear()}.
-          <a href="https://web3templates.com/" target="_blank" rel="noopener">
+          <a href="/" target="_blank" rel="noopener">
             {" "}
             DIAMOND ROOFING
           </a>
-          {/* Illustrations from{" "}
-          <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
-            Glazestock
-          </a> */}
         </div>
       </Container>
-      {/* Do not remove this */}
-      <Backlink />
     </div>
   );
 }
